51530c6dd9f150c50bae4fae4986ed9f4faf6451
Laxmi Kandel <laxmikandel97@gmail.com>
2020-01-06 14:44:53 -0500

    added index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**Name: laxmi kandel
+ * Date: 01/06/2020
+ * URL:http://laxmikandel.greenriverdev.com/IT328/pp1/index.php
+ * Discription: Pair Program 1**/
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 1</title>
+</head>
+<body>
+<h1>Pair Program 1</h1>
+</body>
+</html>
